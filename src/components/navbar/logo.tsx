0044d1b6a624c7d0f147fa01bda291cda811551c
hardwhy <ayi.hardiyanto@dkatalis.com>
2024-11-07 06:31:20 +0700

    feat: setting up Logo and NavSearch

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
+import { LuTent } from "react-icons/lu";
+import Link from "next/link";
 
 function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <Button size={"icon"} asChild>
+      <Link href={"/"}>
+        <LuTent className="w-6 h-6"></LuTent>
+      </Link>
+    </Button>
+  );
 }
 
-export default Logo
+export default Logo;
