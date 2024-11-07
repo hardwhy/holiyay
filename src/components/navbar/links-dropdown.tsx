895c01c97e4ea7527197797a477a4be610305b3a
hardwhy <ayi.hardiyanto@dkatalis.com>
2024-11-07 07:04:35 +0700

    feat: add navbar dropdown menu

--- src/components/navbar/links-dropdown.tsx
+++ src/components/navbar/links-dropdown.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "../ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import { Button } from "../ui/button";
+import UserIcon from "./user-icon";
+import { links } from "@/domain/model/nav-link";
+import Link from "next/link";
 
 function LinksDropDown() {
   return (
-    <div>LinksDropDown</div>
-  )
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"outline"} className="flex gap-4 max-w-[100px]">
+          <LuAlignLeft className="w-6 h-6" />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52" align="start" sideOffset={10}>
+        {links.map((l) => (
+          <DropdownMenuItem key={l.href}>
+            <Link href={l.href} className="capitalize w-full">
+              {l.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
-export default LinksDropDown
+export default LinksDropDown;
---
+++ src/components/navbar/user-icon.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LuUser2 } from "react-icons/lu";
+
+function UserIcon() {
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white">UserIcon</LuUser2>;
+}
+
+export default UserIcon;
