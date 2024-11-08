6deb7dd78376b215ef097abe70b15398e5896dc1
hardwhy <ayi.hardiyanto@dkatalis.com>
2024-11-08 08:36:28 +0700

    chores: update Clerk version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/bookings(.*)',
-  '/checkout(.*)',
-  '/favorites(.*)',
-  '/profile(.*)',
-  '/rentals(.*)',
-  '/reviews(.*)',
+  "/bookings(.*)",
+  "/checkout(.*)",
+  "/favorites(.*)",
+  "/profile(.*)",
+  "/rentals(.*)",
+  "/reviews(.*)",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
