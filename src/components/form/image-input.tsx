04a539f04954ef479b41c270b13594e1a2efd39b
hardwhy <ayi.hardiyanto@dkatalis.com>
2024-11-10 06:53:59 +0700

    fix: typo for ImageInput  name and id

--- src/components/form/image-input.tsx
+++ src/components/form/image-input.tsx
@@ -3,7 +3,7 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 
 export function ImageInput() {
-  const name = "Image";
+  const name = "image";
   return (
     <div className="mb-2">
       <Label htmlFor={name} className="capitalize">
