4503e2caa30627ed6b3575d1cb7e97bfa6e617ca
hardwhy <ayi.hardiyanto@dkatalis.com>
2024-11-10 09:18:19 +0700

    refactor: adjust label to have ReactNode type

--- src/components/form/form-input.tsx
+++ src/components/form/form-input.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 
 type FormInputProps = {
   name: string;
   type: string;
-  label?: string;
+  label?: string | ReactNode;
   defaultValue?: string;
   placeholder?: string;
 };
