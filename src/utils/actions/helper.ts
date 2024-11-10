eca7795085c236e16863324e8b9b8759e200a883
hardwhy <ayi.hardiyanto@dkatalis.com>
2024-11-10 10:11:04 +0700

    refactor: unrelated function as helper

--- src/utils/actions/helper.ts
+++ src/utils/actions/helper.ts
@@ -1,18 +1,4 @@
-import { currentUser, User } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import { ActionResult } from "../types/action-result";
-
-export const getUser = async (): Promise<User> => {
-  const user = await currentUser();
-  if (!user) throw Error("Please login before accessing this menu");
-  return user;
-};
-
-export const getAuthUser = async (): Promise<User> => {
-  const user = await getUser();
-  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
-  return user;
-};
 
 export const handleError = ({
   error,
