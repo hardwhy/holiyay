70a6bf32e2f100d136231b308a4e1449d083f2d9
hardwhy <ayi.hardiyanto@dkatalis.com>
2025-01-29 09:58:30 +0700

    enhance: add Image to property detail page

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -1,5 +1,7 @@
 import FavoriteToggleButton from "@/components/card/favorite-toggle-button";
+import PropertyRating from "@/components/card/property-rating";
 import Breadcrumbs from "@/components/properties/breadcrumbs";
+import ImageContainer from "@/components/properties/image-container";
 import ShareButton from "@/components/properties/share-button";
 import { getPropertyById } from "@/utils/actions/property-actions";
 import { redirect } from "next/navigation";
@@ -12,7 +14,7 @@ type Props = {
 async function PropertyDetailPage({ params: { id } }: { params: Props }) {
   const property = await getPropertyById({ id });
   if (!property) redirect("/");
-  const { bathrooms, bedrooms, beds, guests, name, tagline } = property;
+  const { bathrooms, bedrooms, beds, guests, name, tagline, image } = property;
   const details = { bathrooms, bedrooms, beds, guests };
 
   return (
@@ -26,6 +28,16 @@ async function PropertyDetailPage({ params: { id } }: { params: Props }) {
           <FavoriteToggleButton propertyId={id} />
         </div>
       </header>
+      <ImageContainer mainImage={image} name={name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{name}</h1>
+            <PropertyRating inPage propertyId={id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center"></div>
+      </section>
     </section>
   );
 }
---
+++ src/components/properties/image-container.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  mainImage: string;
+  name: string;
+};
+function ImageContainer({ mainImage, name }: Props) {
+  return (
+    <section className="h-[300px] md:h-[500px] relative mt-8">
+      <Image
+        src={mainImage}
+        fill
+        sizes="100vw"
+        alt={name}
+        className="object-cover rounded"
+        priority
+      />
+    </section>
+  );
+}
+
+export default ImageContainer;
