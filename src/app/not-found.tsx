6173504800c8b2a935f698ccea0d12abf7372bcf
hardwhy <ayi.hardiyanto@dkatalis.com>
2025-02-06 06:37:30 +0700

    feat: add 404 Not Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h2 className="text-2xl font-bold mb-2">404 - Page Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+      </div>
+    </div>
+  );
+}
